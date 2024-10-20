405e002f316a0c316a246aadd7594e30d669967a
williamsgqdev <williamsagbunu@gmail.com>
2024-10-20 15:33:58 -0500

    get current mac address method

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def get_args():
     parser = optparse.OptionParser()
@@ -20,5 +21,20 @@ def mac_changer(interface, new_mac):
     subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["sudo", "ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["sudo", "ifconfig", interface]).decode("utf-8")
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read mac address")
+
 opt = get_args()
+current_mac = get_current_mac(opt.interface)
+print("Current Mac "+ str(current_mac))
 mac_changer(opt.interface, opt.new_mac)
+current_mac = get_current_mac(opt.interface)
+if current_mac == opt.new_mac:
+    print("[+] MAC address was successfully changed to "+ opt.new_mac)
+else:
+    print("[-] MAC address did not get changed")
