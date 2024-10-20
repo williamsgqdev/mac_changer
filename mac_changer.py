cca65fdaa7dd2f980b6c9b75f1d7492c9ba2a4e9
williamsgqdev <williamsagbunu@gmail.com>
2024-10-20 08:02:56 -0500

    refactor code to watch for errors

--- mac_changer.py
+++ mac_changer.py
@@ -3,17 +3,22 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface use --help for info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a mac address use --help for info")
+    return options
 
-parser.add_option("-i", "--interface", dest= "interface", help="Interface to change its MAC Address")
-parser.add_option("-m", "--mac", dest= "new_mac", help="New Mac Address")
-(options, arguments) = parser.parse_args()
+def mac_changer(interface, new_mac):
+    print("[+] Changing mac address for " + interface + " to " + new_mac)
+    subprocess.call(["sudo", "ifconfig", interface, "down"])
+    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
 
-interface = options.interface
-new_mac = options.new_mac
-
-print("[+] Changing mac address for "+ interface + " to " + new_mac)
-
-subprocess.call(["sudo","ifconfig", interface, "down"])
-subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["sudo", "ifconfig", interface , "up"])
+opt = get_args()
+mac_changer(opt.interface, opt.new_mac)
